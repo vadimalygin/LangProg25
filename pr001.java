b2caa88f71efc4734893283315f5898ba05d68be
vadimalygin <vadim.malygin.2001@mail.ru>
2025-10-22 10:00:04 +0700

    Added a Gen class instance of String type

--- pr001.java
+++ pr001.java
@@ -1,12 +1,16 @@
 class Gen<T> {
+	//Declaring a general class type
 	T ob;
+	//Defining class constructor
+	//assigning generalized object as a parameter
 	Gen(T o){
 		ob = o;
 	}
-
+	//Method that returns class object
 	T getOb() {
 		return ob;
 	}
+	//Method that returns the name of a class type 
 	void showType() {
 		System.out.println("Type of T: " + ob.getClass().getName());
 	}
@@ -14,10 +18,24 @@ class Gen<T> {
 
 class pr001 {
 	public static void main(String args[]) {
+		//Creating a class pointer with object type Integer
 		Gen<Integer> iob;
+		//Assigning a new class instance of type Integer to our pointer 
 		iob = new Gen<Integer>(88);
+		//Showing object Type
 		iob.showType();
+		//Assigning object value to a variable
 		int v = iob.getOb();
-		System.out.println("Value: " + v);
+		System.out.println("Value: " + v + "\n");
+		
+
+		//Creating a class pointer with object type String
+		//Assigning a new class instance of type String to our pointer 
+		Gen<String> strob = new Gen<String>("String of generalized class");
+		//Showing object Type
+		strob.showType();
+		//Assigning object value to a variable
+		String str = strob.getOb();
+		System.out.println("Value: " + str + "\n");
 	}
 }
