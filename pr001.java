368210ea60d3d9aca0340feeb72e6e16b86969fe
vadimalygin <vadim.malygin.2001@mail.ru>
2025-10-22 11:00:53 +0700

    Added a TwoGen class instance of Integer & String type

--- pr001.java
+++ pr001.java
@@ -16,6 +16,29 @@ class Gen<T> {
 	}
 }
 
+class TwoGen<T, V> {
+	//Declaring a general class type
+        T ob1;
+	V ob2;
+	//Defining class constructor
+	//assigning generalized object as a parameter
+        TwoGen(T o1, V o2){
+		ob1 = o1;
+		ob2 = o2;
+	}
+	T getOb1() {
+		return ob1;
+	}
+	V getOb2() {
+		return ob2;
+	}
+	//Method that returns the name of a class type
+	void showTypes() {
+		System.out.println("Type of T: " + ob1.getClass().getName());
+		System.out.println("Type of V: " + ob2.getClass().getName());
+	}
+}
+
 class pr001 {
 	public static void main(String args[]) {
 		//Creating a class pointer with object type Integer
@@ -37,5 +60,15 @@ class pr001 {
 		//Assigning object value to a variable
 		String str = strob.getOb();
 		System.out.println("Value: " + str + "\n");
+
+		////////////////////////////////////////////////////////////////////////////
+
+		TwoGen<Integer, String> tgob = new TwoGen<Integer, String>(89, "twogen string");
+		tgob.showTypes();
+		v = tgob.getOb1();
+		str = tgob.getOb2();
+
+		System.out.println("Value 1: " + v);
+		System.out.println("Value 2: " + str + "\n");
 	}
 }
